f71e791ca8ac40b8d5c34e605fc5f535e75c0d6d
SauloDc <marcielle0644@gmail.com>
2024-10-18 00:19:55 -0300

    criando view email

---
+++ resources/views/emails/vendas.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Venda Realizada</title>
+</head>
+
+<body>
+    <h1>Venda Realizada com Sucesso!</h1>
+    <p>Olá, tudo bem?</p>
+    <p>É possível acompanhar através do site: </p>
+    <p>Link: http://127.0.0.1:5500/</p>
+    <p>Código: {{ $venda->codigo }}</p>
+
+</body>
+
+</html>
