3a4fe46668234af8f5f4e0cc83e60ce53e51e191
SauloDc <marcielle0644@gmail.com>
2024-10-18 22:44:26 -0300

    ajuste do componente butao

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,13 @@
-<!-- resources/views/components/button.blade.php -->
-@props(['type' => 'button', 'class' => '', 'href' => '#'])
+@props(['type' => 'button', 'class' => '', 'href' => '#', 'onclick' => ''])
 
-<a href="{{ $href }}" {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition']) . ' ' . $class }}">
-    {{ $slot }}
-</a>
+@if ($onclick)
+    <button type="{{ $type }}" onclick="{{ $onclick }}"
+        {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition ' . $class]) }}>
+        {{ $slot }}
+    </button>
+@else
+    <button href="{{ $href }}"
+        {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition ' . $class]) }}>
+        {{ $slot }}
+    </button>
+@endif
