8af0ae3bb1851205977da89903cc2e5046ad689b
SauloDc <marcielle0644@gmail.com>
2024-10-18 22:47:35 -0300

    ajustes de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VendaController;
-use App\Http\Controllers\VendaProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -24,22 +19,30 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/produto/create', [ProdutoController::class, 'create'])->name('produtos.create');
     Route::post('/produto', [ProdutoController::class, 'store'])->name('produtos.store');
-    Route::get('/produtos', [ProdutoController::class, 'index']);
+    Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
     Route::get('/produto/{id}', [ProdutoController::class, 'show']);
     Route::put('/produto/{id}', [ProdutoController::class, 'update']);
     Route::delete('/produto/{id}', [ProdutoController::class, 'destroy']);
-    Route::get('/vendas/create', [VendaController::class, 'create']);
+    Route::get('/dashboard', [VendaController::class, 'create'])->name('dashboard');
     Route::get('vendas/', [VendaController::class, 'index'])->name('venda');
     Route::post('vendas/produtos/', [VendaController::class, 'store'])->name('venda.store');
+
+    Route::get('clientes', [ClienteController::class, 'index']);
+    Route::post('clientes', [ClienteController::class, 'store']);
+    Route::get('clientes/{id}', [ClienteController::class, 'show']);
+    Route::put('clientes/{id}', [ClienteController::class, 'update']);
+    Route::delete('clientes/{id}', [ClienteController::class, 'destroy']);
+
+
+    Route::get('categoria', [CategoriaController::class, 'index'])->name('categoria.index');
+    Route::post('categoria', [CategoriaController::class, 'store'])->name('categoria.store');
+    Route::get('categoria/{id}', [CategoriaController::class, 'show'])->name('categoria.show');
+    Route::put('categoria/{id}', [CategoriaController::class, 'update'])->name('categoria.upadate');
+    Route::delete('categoria/{id}', [CategoriaController::class, 'destroy']);
 });
 
 
 
-Route::get('clientes', [ClienteController::class, 'index']);
-Route::post('clientes', [ClienteController::class, 'store']);
-Route::get('clientes/{id}', [ClienteController::class, 'show']);
-Route::put('clientes/{id}', [ClienteController::class, 'update']);
-Route::delete('clientes/{id}', [ClienteController::class, 'destroy']);
 
 
 
