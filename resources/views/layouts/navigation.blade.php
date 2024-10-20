d4d8674ec8793831474f8073f90ba4b2809367bb
SauloDc <marcielle0644@gmail.com>
2024-10-20 02:36:45 -0300

    ajustes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('categoria.index')" :active="request()->routeIs('categoria.index')">
+                    <x-nav-link :href="route('categorias.index')" :active="request()->routeIs('categoria.index')">
                         {{ __('Categoria') }}
                     </x-nav-link>
                 </div>
