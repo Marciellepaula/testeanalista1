54617371d39123dd7f3c5ea73117b55e9eb23fb7
SauloDc <marcielle0644@gmail.com>
2024-10-20 02:39:14 -0300

    adicionando quantidade_estoqueao modal

--- resources/views/components/carrinho-modal.blade.php
+++ resources/views/components/carrinho-modal.blade.php
@@ -1,16 +1,17 @@
 @props(['produtos'])
 
-
 <div id="addToCartModal" class="hidden fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
     <div class="bg-white p-4 rounded-lg">
         <h2 class="text-lg font-semibold" id="modalProductName">Adicionar ao Carrinho</h2>
         <select id="product" class="mt-2 border rounded p-2">
             @foreach ($produtos as $produto)
-                <option value="{{ $produto->id }}" data-price="{{ $produto->preco_venda }}">
+                <option value="{{ $produto->id }}" data-preco="{{ $produto->preco_venda }}"
+                    data-quantidade_estoque="{{ $produto->quantidade_estoque }}">
                     {{ $produto->nome }}
                 </option>
             @endforeach
         </select>
+
         <input id="quantidade" type="number" class="mt-2 border rounded p-2" placeholder="Quantidade" min="1">
         <button class="mt-4 bg-green-500 text-white px-4 py-2 rounded" onclick="addToCart()">Adicionar</button>
         <button class="mt-4 bg-red-500 text-white px-4 py-2 rounded" onclick="closeAddToCartModal()">Fechar</button>
