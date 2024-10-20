436d76b0b0ea1fa2b6720533a668cb81f15b8232
SauloDc <marcielle0644@gmail.com>
2024-10-20 14:22:35 -0300

    adicionando user id ao cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -9,10 +9,16 @@ class Cliente extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['nome', 'cpf', 'telefone', 'email'];
+    protected $fillable = ['nome', 'cpf', 'telefone', 'email', 'user_id'];
 
     public function vendas()
     {
         return $this->hasMany(Venda::class);
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
